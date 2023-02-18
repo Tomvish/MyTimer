fddef8263fd6ed16cca44430f09d648294a36f15
Art Vish <svolkamy@gmail.com>
2023-02-18 19:56:36 +0300

    Периоды 25-работа, 5 - отдых, если пользователь не ввёл ничего. Теперь отдых вкл в любую секунду по истечении ждёт 'старт'.

--- MyTimer.py
+++ MyTimer.py
@@ -6,22 +6,22 @@ import  re
 
 index = 1  # 60 - минуты, 1 - секунды для проверки
 step = False #True
-work_time = 1
+work_time = 1  # Уст. по-умолчанию в функции start.
 rest_time = 1
 work_day = 1    # час
 #################
-def option():
-    tk.messagebox.askyesnocancel('Укажите время', f"запустить {spin}")
+# def option():
+#     tk.messagebox.askyesnocancel('Укажите время', f"запустить {spin}")
 
 def is_valid(newal):
     # try:
-    result = re.match("\d$", newal) is not None # TODO настроить проверку более одной цифры
-    if not result or len(newal) >=4:
+    result = re.match("\d{0,3}$", newal)# is not None # TODO настроить проверку более одной цифры
+    if not result or len(newal) >=3:
         errmsg=("Рекомендуется делать перерывы")
         print("Рекомендуется делать перерывы")
         err_lbl.configure(text=(errmsg), bg='orange',)
     else:
-        err_lbl.configure(text="норм", )
+        err_lbl.configure(text=newal, )
     return result
     # except:
     #     err_lbl.configure(text=(errmsg), bg='orange')
@@ -29,24 +29,32 @@ def is_valid(newal):
     #     return result
 
 def start():
-    global  step
-    step = not step
+    global step
+    step = not step # старт/отдых
     work_time = (ent_work.get()) #int(s))
-    work_time = int(work_time)
-    print(f'Тип: {type(work_time)} введено: {work_time}')
+    if work_time == "":
+        work_time = 25 # Это работа по-умолчанию, если не введено пользователем.
+    else:
+        work_time = int(work_time)
     rest_time = ent_rest.get()
-    rest_time = int(rest_time)
+    if rest_time == "":
+        rest_time = 5 # Это отдых по-умолчанию, если не введено пользователем.
+    else:
+        rest_time = int(rest_time)
+
+    # Обработка клика кнопки старт/отдых
     if step: # работа
-        btnStart.configure(text='Пауза', bg='red', default='active',) # command=option) #text[1]) #
+        print(f'Тип: {type(work_time)} введено: {work_time}') ### при if
+        btnStart.configure(text='Отдых', bg='red', default='active',) # command=option) #text[1]) #
         lbl.configure(text=('Удачи в работе!'))
         ent_work.configure(bg='yellow')
         ent_rest.configure(bg='white')
         timer(work_time, rest_time)
-
     else: # отдых
+        print(f'Тип: {type(rest_time)} введено: {rest_time}') ### при else
         btnStart.configure(text='Старт', bg='yellow', default='normal') #text[0]) #
         lbl.configure(text=('Отдохни!'))
-        print("Пауза,'Отдохни!' ", elapse_rest_time)
+        print("Пауза,'Отдохни!' ") #, elapse_rest_time) ###
         ent_work.configure(bg='white')
         ent_rest.configure(bg='green')
         # timer(work_time, rest_time)
@@ -57,43 +65,49 @@ def timer(work_time, rest_time):
 
     while step:
         lbl.configure(text=('Начинаем работу!'), bg='orange')
+        print('Начинаем работу!') ###
         start_time = time.time()
 
-        # Запустите таймер для рабочего интервала.
+        # Запустим таймер для рабочего интервала.
         start_work = time.time()
         elapse_work_time = 0
-        while elapse_work_time <= work_time * index -1: #7: #
+        while elapse_work_time <= work_time * index -1 and step: #7: #
             elapse_work_time = time.time() - start_work
             mins, secs = divmod(work_time * index - elapse_work_time, index)
 
-            txt = f'{int(mins)}:{int(secs)}' # Ещё поработать
+            txt = f'{int(mins)}:{int(secs)}' # Табло Ещё поработать
             lbl_tablo.configure(text=txt)
+            print(txt)
             time.sleep(1)
             win.update()
         lbl.configure(text=("Время отдохнуть!"), bg='green')
-        print("Время отдохнуть!",elapse_work_time )
-        print(step, '1')
+        print("Время отдохнуть! в работе прошло: ", elapse_work_time )
+        print(step, 'work') ###
 
         # Запустите таймер для интервала отдыха.
         start_rest = time.time()  # Отмерять отдых с этой секунды
         elapse_rest_time = 0  # установить истёкшее время = 0
+        print('старт отдыха:', start_rest)
         while elapse_rest_time <= rest_time * index :
             elapse_rest_time = time.time() - start_rest -1
             mins, secs = divmod(rest_time * index - elapse_rest_time+1, index)
-            txt = f'{int(mins)}:{int(secs)}' # \nЕщё отдохнуть
+            txt = f'{int(mins)}:{int(secs)}' # Табло Ещё отдохнуть
             lbl_tablo.configure(text=txt)
+            print(txt)
             time.sleep(1)
             win.update()
         lbl.configure(text=("Отдых окончен!"))
-        print('"Отдых окончен!"', elapse_rest_time )
+        print('"Отдых окончен!"', elapse_rest_time, 'end:', time.time() ) ###
+        print(step, 'rest')
         # step = not step
         ########################
 win = tk.Tk()
+win.title("Мой таймер")
 win.attributes('-toolwindow', True)
 # win.columnconfigure(3, minsize=3)
 # win.rowconfigure(1, minsize=10)
 win.geometry('235x152-150-205')  #######
-print(win.geometry())
+# print(win.geometry())
 
 # spin = tk.Spinbox(messagebox, from_=0, to=100)
 # var = IntVar()
@@ -112,7 +126,7 @@ btnStart = tk.Button(frmb, text='<<< Дело-Отдых >>>', width=20, command
 check = (win.register(is_valid), "%P")
 
 ent_work = tk.Entry(frmb, validate='key', validatecommand=check, width=5)
-ent_rest = tk.Entry(frmb, width=5)
+ent_rest = tk.Entry(frmb, validate='key', validatecommand=check, width=5)
 
 frm.pack(fill=tk.Y,  side=tk.TOP,)
 frmb.pack(fill=tk.Y,  side=tk.BOTTOM,)
